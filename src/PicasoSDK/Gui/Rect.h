488c3d0f72962def70ad2a36a390faa01c7e4fad
juliencombattelli <julien.combattelli@hotmail.com>
2017-04-21 08:03:08 +0200

    Add constructors to Point, Size and Rect classes
    Rect::is_inside(Point,Rect) is rename Rect::contains(Point) and is no
    longer static

--- src/PicasoSDK/Gui/Rect.h
+++ src/PicasoSDK/Gui/Rect.h
@@ -19,29 +19,44 @@ namespace Picaso
 
 struct Point
 {
+	Point() : x(0), y(0) {}
+	Point(uint16_t _x, uint16_t _y) : x(_x), y(_y) {}
+	Point(const Point& point) : x(point.x), y(point.y) {}
+	Point& operator=(const Point& point) { x = point.x; y = point.y; return *this; }
+
     uint16_t x;
     uint16_t y;
 };
 
 struct Size
 {
+	Size() : width(0), height(0) {}
+	Size(uint16_t _width, uint16_t _height) : width(_width), height(_height) {}
+	Size(const Size& size) : width(size.width), height(size.height) {}
+	Size& operator=(const Size& size) { width = size.width; height = size.height; return *this; }
+
 	uint16_t width;
 	uint16_t height;
 };
 
 struct Rect
 {
-    Point origin;
-    Size size;
+	Rect() = default;
+	Rect(const Point& _origin, const Size& _size) : origin(_origin), size(_size) {}
+	Rect(const Rect& rect) : origin(rect.origin), size(rect.size) {}
+	Rect& operator=(const Rect& rect) { origin = rect.origin; size = rect.size; return *this; }
 
-    static bool is_inside(const Point& point, const Rect& rect)
+    bool contains(const Point& point)
     {
-        if( (point.x > rect.origin.x && point.x < rect.origin.x + rect.size.width) &&
-        	(point.y > rect.origin.y && point.y < rect.origin.y + rect.size.height))
+        if( (point.x > origin.x && point.x < origin.x + size.width) &&
+        	(point.y > origin.y && point.y < origin.y + size.height))
             return true;
         else
             return false;
     }
+
+    Point origin;
+    Size size;
 };
 
 } // namespace Picaso
