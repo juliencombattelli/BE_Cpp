fa56e662f040011d8cf4ec79a304ef22d88d281f
guillaume <guillaum324bis@hotmail.fr>
2017-04-25 12:07:45 +0200

    Add window_Base and Touchable_Window

---
+++ src/PicasoSDK/Gui/Touchable_Window.h
@@ -0,0 +1,32 @@
+/*
+ * Touchable_Window.h
+ *
+ *  Created on: 25 avr. 2017
+ *      Author: guill
+ */
+
+#ifndef PICASOSDK_GUI_TOUCHABLE_WINDOW_H_
+#define PICASOSDK_GUI_TOUCHABLE_WINDOW_H_
+
+#include "PicasoSDK/Gui/Window_Base.h"
+#include "PicasoSDK/Gui/touch_dispatcher.h"
+
+namespace Picaso
+{
+
+class Touchable_Window : public Window_Base
+{
+public:
+	Touchable_Window(Serial_Commander& lcd) : Window_Base(lcd) {}
+	virtual ~Touchable_Window() = default;
+
+	virtual void show() = 0;
+	void update() {m_touch_dispatcher.touch_periodic_task(m_lcd); custom_update(); }
+protected:
+	virtual void custom_update() {}
+	Touch_Dispatcher m_touch_dispatcher;
+};
+
+} // namespace Picaso
+
+#endif /* PICASOSDK_GUI_TOUCHABLE_WINDOW_H_ */
---
+++ src/PicasoSDK/Gui/Window_Base.h
@@ -0,0 +1,33 @@
+/*
+ * Window_Base.h
+ *
+ *  Created on: 25 avr. 2017
+ *      Author: guill
+ */
+
+#ifndef PICASOSDK_GUI_WINDOW_BASE_H_
+#define PICASOSDK_GUI_WINDOW_BASE_H_
+
+#include "PicasoSDK/LcdDriver/Serial_Commander.h"
+
+namespace Picaso
+{
+
+class Window_Base
+{
+public:
+	Window_Base(Serial_Commander& lcd) : m_lcd(lcd) {}
+	virtual ~Window_Base() = default;
+
+	virtual void show() = 0;
+	virtual void update() {}
+
+private:
+	Serial_Commander& m_lcd;
+};
+
+} // namespace Picaso
+
+
+
+#endif /* PICASOSDK_GUI_WINDOW_BASE_H_ */
