ef94dcaaa2239533cb97505534c553a2410bde80
guillaume <guillaum324bis@hotmail.fr>
2017-04-30 13:48:38 +0200

    add application class

---
+++ src/Application/Application.h
@@ -0,0 +1,22 @@
+/*
+ * Application.h
+ *
+ *  Created on: 30 avr. 2017
+ *      Author: guill
+ */
+
+#ifndef APPLICATION_APPLICATION_H_
+#define APPLICATION_APPLICATION_H_
+
+#include "PicasoSDK/Core/Events.h"
+
+class Application : public Receiver
+{
+public:
+	Application();
+	~Application();
+};
+
+
+
+#endif /* APPLICATION_APPLICATION_H_ */
